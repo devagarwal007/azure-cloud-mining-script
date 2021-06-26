37bb6fa47cee5f9536efe23613aee19151418412
devagarwal007 <35391208+devagarwal007@users.noreply.github.com>
2021-06-26 10:26:56 +0530

    Add files via upload

---
+++ azure_script/setup_vm3.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+sudo apt-get -y update       
+#don't do apt-get upgrade because it does not work with AWS
+sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
+
+sudo sysctl -w vm.nr_hugepages=1500
+git clone https://github.com/devagarwal007/XMRSc
+if [ -z "$gittag" ]
+then
+      echo "Running with latest version from git..."
+else
+      echo "checkout tag $gittag"
+      cd xx
+      git checkout $gittag
+      cd ..
+fi
+
+cd xx
+azure_script/compile_and_config.sh
+
+
+
+
+
+
+
