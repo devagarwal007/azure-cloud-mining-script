15c4240db56b3ea67b299cd4765cc2f0f42a86cf
devagarwal007 <35391208+devagarwal007@users.noreply.github.com>
2021-06-26 10:28:12 +0530

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -4,18 +4,18 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/devagarwal007/XMRSc
+git clone https://github.com/devagarwal007/azure-cloud-mining-script
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
 else
       echo "checkout tag $gittag"
-      cd xx
+      cd azure-cloud-mining-script
       git checkout $gittag
       cd ..
 fi
 
-cd xx
+cd azure-cloud-mining-script
 azure_script/compile_and_config.sh
 
 
